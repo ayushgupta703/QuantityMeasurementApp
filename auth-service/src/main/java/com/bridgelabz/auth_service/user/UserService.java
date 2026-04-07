cbcc078fc94f6f9e70f7d5393c4832f02447a3b5
Ayush Gupta <ag6691602@gmail.com>
2026-04-07 23:39:44 +0530

    [Ayush Gupta] UC22 Deployement Configs

--- auth-service/src/main/java/com/bridgelabz/auth_service/user/UserService.java
+++ auth-service/src/main/java/com/bridgelabz/auth_service/user/UserService.java
@@ -16,9 +16,6 @@ public class UserService {
     }
 
     public User register(User user) {
-        System.out.println("USER: " + user);
-        System.out.println("PASSWORD: " + user.getPassword());
-        
         if (userRepository.findByEmail(user.getEmail()).isPresent()) {
             throw new RuntimeException("Email already exists");
         }
